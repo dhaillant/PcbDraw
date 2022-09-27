d546612ac5a8798ea2c1ff9787181dbb1bb643d8
Electro707 <general@electro707.com>
2022-09-27 14:40:18 -0400

    Improved the read_resistance function

--- pcbdraw/unit.py
+++ pcbdraw/unit.py
@@ -15,6 +15,8 @@ def erase(string: str, what: List[str]) -> str:
 def read_resistance(value: str) -> Decimal:
     """
     Given a string, try to parse resistance and return it as Ohms (Decimal)
+
+    This function can raise a decimal.InvalidOperation if the value is invalid
     """
     p_value = erase(value, ["Ω", "Ohms", "Ohm"]).strip()
     p_value = p_value.replace(" ", "") # Sometimes there are spaces after decimal place
@@ -25,15 +27,14 @@ def read_resistance(value: str) -> Decimal:
         "M": [Decimal(1e6), Decimal(1e3)],
         "G": [Decimal(1e9), Decimal(1e6)]
     }
-    try:
-        numerical_value = None
-        for prefix, table in unit_prefixes.items():
-            if prefix in p_value:
-                split = [Decimal(x) if x != "" else Decimal(0) for x in p_value.split(prefix)]
-                numerical_value = split[0] * table[0] + split[1] * table[1]
-                break
-        if numerical_value is not None:
-            return numerical_value
-    except Exception:
-        pass
-    raise ValueError(f"Cannot parse '{value}' to resistance")
+    numerical_value = None
+    for prefix, table in unit_prefixes.items():
+        if prefix in p_value:
+            split = [Decimal(x) if x != "" else Decimal(0) for x in p_value.split(prefix)]
+            numerical_value = split[0] * table[0] + split[1] * table[1]
+            break
+    if numerical_value is None:
+        # Handle values that don't have a prefix. If this fails, a InvalidOperation will be raised
+        #       which will be catched by the exception
+        numerical_value = Decimal(p_value)
+    return numerical_value
